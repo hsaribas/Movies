48a400df368e627b9c135509e1ddc279bf88b7db
Huzeyfe Saribas <huzeyfeorhan77@gmail.com>
2023-07-14 18:47:16 +0300

    commit-3

--- src/main/java/dev/huz/movies/service/MovieService.java
+++ src/main/java/dev/huz/movies/service/MovieService.java
@@ -1,7 +1,25 @@
 package dev.huz.movies.service;
 
+import dev.huz.movies.model.Movie;
+import dev.huz.movies.repository.MovieRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> allMovies() {
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singeMovie(ObjectId id) {
+        return movieRepository.findById(id);
+    }
 }
